c966d50fd120490c9fe9fa2245f73dfb78a2cf73
malkyfried <hand1b22@bjcomp.org>
2023-07-17 17:21:16 +0300

    [IMS-152]-update search

--- ims-client/src/components/search/search.tsx
+++ ims-client/src/components/search/search.tsx
@@ -1,5 +1,6 @@
 import SearchIcon from '@mui/icons-material/Search';
 import IconButton from '@mui/material/IconButton';
+import FilterListIcon from '@mui/icons-material/FilterList';
 import InputBase from '@mui/material/InputBase';
 import Paper from '@mui/material/Paper';
 import * as React from 'react';
@@ -28,16 +29,17 @@ const Search: React.FC<EventProps> = ({ onEvent, setValue }) => {
             component="form"
             sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
         >
+            <IconButton type="button" sx={{ p: '10px' }} aria-label="search" onClick={handleClick}>
+            <SearchIcon />
+            </IconButton>
             <InputBase
                 sx={{ ml: 1, flex: 1 }}
-                placeholder="Search Incident"
+                placeholder="Search..."
                 inputProps={{ 'aria-label': 'search incident' }}
                 onKeyDown={handleKeyDown}
                 onChange={handleChange}
             />
-            <IconButton type="button" sx={{ p: '10px' }} aria-label="search" onClick={handleClick}>
-                <SearchIcon />
-            </IconButton>
+          <FilterListIcon sx={{ opacity: "20%" }}></FilterListIcon>
         </Paper>
     );
 };
