5aa39133ec7236ea8a4b043e964cb86d38a82ae5
tamarmiller1 <tamarm@darkeychana.com>
2023-07-17 18:22:51 +0300

    [IMS-13-C] - add succesful

--- ims-client/src/service/apiCalls.ts
+++ ims-client/src/service/apiCalls.ts
@@ -1,11 +1,13 @@
 import axios from "axios"
-import  IIncident from "../interface/incidentInterface"
+import IIncident from "../interface/incidentInterface"
 
 const apiCalls = {
     //להוסיף את כל הקריאות שרת
     getIncidents: () => axios.get(`http://127.0.0.1:7000/incident`).then(response => response.data),
-    getAggregation:() => axios.get(`http://127.0.0.1:7000/aggregation`).then(response => response.data),
-    createIncident:(incident:IIncident) => axios.post(`http://127.0.0.1:7000/incident`,incident).then(response => response.data)
+    getAggregation: () => axios.get(`http://127.0.0.1:7000/aggregation`).then(response => response.data),
+    createIncident: (incident: IIncident) => axios.post(`http://127.0.0.1:7000/incident/addIncident`, incident).then(response => response.data),
+    getTags: () => axios.get(`http://127.0.0.1:7000/tag`).then(response => response.data),
+
 }
 
 export default apiCalls
