c621b2cc762c07c109954cada30f9203a788f20e
Rivky Fisher <hand1b20@mbjcomp.org.il>
2023-07-20 14:14:52 +0300

    add bucket name

--- ims-server/src/aws_s3/servicesS3.ts
+++ ims-server/src/aws_s3/servicesS3.ts
@@ -1,16 +1,24 @@
 import AWS from 'aws-sdk';
 import fs from 'fs';
 import path from 'path';
+import dotenv from 'dotenv';
+import logger from "../loggers/log";
+
+dotenv.config()
+
 
 const s3 = new AWS.S3();
 export default class ServicesS3 {
-    static uploadFileToS3 = async (bucketName: string, fileName: string, filePath: string) => {
+    static uploadFileToS3 = async ( fileName: string, filePath: string) => {
+        if (!process.env.BUCKET_NAME) {
+            logger.error('BUCKET_NAME environment variable is not set.');
+            return;
+          }
         const params = {
-            Bucket: bucketName,
+            Bucket: process.env.BUCKET_NAME,
             Key: fileName,
             Body: require('fs').createReadStream(filePath)
         };
-
         try {
             const result = await s3.upload(params).promise();
             console.log('File uploaded successfully:', result.Location);
@@ -20,16 +28,19 @@ export default class ServicesS3 {
     }
 
 
-    static downloadFileFromS3 = async (bucketName: string, fileName: string, saveAsFileName: string) => {
+    static downloadFileFromS3 = async (fileName: string, saveAsFileName: string) => {
+        if (!process.env.BUCKET_NAME) {
+            logger.error('BUCKET_NAME environment variable is not set.');
+            return;
+          }
         const params = {
-            Bucket: bucketName,
+            Bucket: process.env.BUCKET_NAME,
             Key: fileName
         };
 
         try {
             const downloadsDirectory = require('os').homedir() + '/Downloads';
             const filePath = path.join(downloadsDirectory, saveAsFileName);
-
             const fileStream = fs.createWriteStream(filePath);
             const data = await s3.getObject(params).promise();
             fileStream.write(data.Body);
@@ -39,9 +50,13 @@ export default class ServicesS3 {
             console.error('Error downloading file from S3:', error);
         }
     }
-    static deleteFileFromS3 = async (bucketName: string, fileName: string) => {
+    static deleteFileFromS3 = async ( fileName: string) => {
+        if (!process.env.BUCKET_NAME) {
+            logger.error('BUCKET_NAME environment variable is not set.');
+            return;
+          }
         const params = {
-            Bucket: bucketName,
+            Bucket: process.env.BUCKET_NAME,
             Key: fileName
         };
 
@@ -52,16 +67,25 @@ export default class ServicesS3 {
             console.error('Error downloading file from S3:', error);
         }
     }
-    static showFileFromS3 = async (bucketName: string, fileName: string) => {
+
+    
+    static showFileFromS3 = async (fileName: string) => {
+        if (!process.env.BUCKET_NAME) {
+            logger.error('BUCKET_NAME environment variable is not set.');
+            return;
+          }
         const params = {
-            Bucket: bucketName,
+            Bucket: process.env.BUCKET_NAME,
             Key: fileName
         };
         try {
             const data = await s3.getObject(params).promise();
             return data.Body;
         } catch (error) {
-            console.error('Error downloading file from S3:', error);
+            logger.error('Error downloading file from S3:', error);
         }
     }
+
+
+      
 }
--- ims-server/src/controllers/IncidentController.ts
+++ ims-server/src/controllers/IncidentController.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-
 import { IIncident } from '../interfaces/IncidentInterface';
 import incidentService from '../services/incidentService';
 import { constants } from '../loggers/constants';
