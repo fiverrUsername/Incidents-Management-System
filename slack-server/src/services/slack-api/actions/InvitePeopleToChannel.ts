bc0c0676ccf6e53c8ee528da931ad7e8ffdc9f19
Rut1095 <rutlandau@gmail.com>
2023-07-23 15:02:42 +0300

    [IMS-186] - fix invite

--- slack-server/src/services/slack-api/actions/InvitePeopleToChannel.ts
+++ slack-server/src/services/slack-api/actions/InvitePeopleToChannel.ts
@@ -5,6 +5,6 @@ export async function InvitePeopleToChannel(channelId:string,users:string[]){
         // Channel ID of the new channel
         channel: channelId,
         // User ID of yourself
-        users: users
+        users: users.join(",")
       });
 }
