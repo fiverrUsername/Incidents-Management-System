1f2a153264c824403ca726729b75235b9f08f99e
Rut1095 <rutlandau@gmail.com>
2023-07-25 11:53:15 +0300

    [IMS-186-B] - send message on creat timeline

---
+++ slack-server/src/app.ts
@@ -0,0 +1,20 @@
+import { getUsers } from "./services/slack-api/actions/getUsers"
+import { sendMessageFromBot } from "./services/slack-api/actions/sendMessageFromBot"
+import { addTimeLineEvent, ITimelineEvent } from "./services/slack-api/wrap/sendTimeLine"
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = 4700;
+
+app.use(bodyParser.json());
+app.get('/',()=>console.log("ssts"))
+app.post('/', (req:Request, res:Response) => {
+    console.log(req.body)
+    addTimeLineEvent(req.body);
+    res.json({ message: 'Timeline event added successfully'});
+  });
+  
+  app.listen(port, () => {
+    console.log(`Server A listening on port ${port}`);
+  });
