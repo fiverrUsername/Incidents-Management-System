4447c9b255aa05a296f89df4b837c423eb0a5711
אביטל פינץ <avital058pz@gmail.com>
2023-07-25 10:16:30 +0300

    [IMS-152]- update summary

--- ims-client/src/components/summary/displaySummary.tsx
+++ ims-client/src/components/summary/displaySummary.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import { StyledBox, StyledPaper } from '../../pages/timeLine/timeLinePage.style'
+import { CustomScrollbar, StyledBox, StyledPaper } from '../../pages/timeLine/timeLinePage.style'
 import { ISummary } from '../../interface/ISummary';
-import { Grid, colors } from '@mui/material';
+import {  Box, Chip, Grid, colors } from '@mui/material';
 import theme from '../../theme';
 import dayjs from 'dayjs';
+import { StyledChip } from './displaySummary.style';
+import { BiBorderRadius } from 'react-icons/bi';
 
 
 
@@ -11,30 +13,35 @@ interface propsDisplaySummary {
     summaryIncident: ISummary
 }
 const DisplaySummary = ({ summaryIncident }: propsDisplaySummary) => {
-    
+
+ 
+ 
+  
+ 
     const date = dayjs(summaryIncident.createdAt).format("DD/MM/YYYY")
     return (
         <StyledPaper>
             <Grid container direction="row" justifyContent="center" alignItems="flex-start" flexWrap="nowrap">
                 <Grid container direction="column" justifyContent="flex-start" alignItems="flex-start">
-                    <StyledBox >Created by:<label style={{color:theme.palette.primary.dark ,fontSize:theme.typography.fontSize}}>{summaryIncident.createdBy}</label></StyledBox>
+                    <StyledBox >Created by:<label style={{ color: theme.palette.primary.dark, fontSize: theme.typography.fontSize }}>{summaryIncident.createdBy}</label></StyledBox>
                     <StyledBox>Created at:
-                      <label style={{color:theme.palette.primary.dark ,fontSize:theme.typography.fontSize}}> {date.toString()}</label> 
+                        <label style={{ color: theme.palette.primary.dark, fontSize: theme.typography.fontSize }}> {date.toString()}</label>
                     </StyledBox>
                 </Grid>
                 <Grid container direction="column" justifyContent="flex-start" alignItems="flex-start">
-                    <StyledBox>Current priority: <label style={{color:theme.palette.primary.dark ,fontSize:theme.typography.fontSize}}>{summaryIncident.currentPriority}</label></StyledBox>
+                    <StyledBox>Current priority: <label style={{ color: theme.palette.primary.dark, fontSize: theme.typography.fontSize }}>{summaryIncident.currentPriority}</label></StyledBox>
                 </Grid>
 
                 <Grid container direction="column" justifyContent="flex-start" alignItems="flex-start">
-                    
-                   {summaryIncident.tags&&<StyledBox>Affected services: {summaryIncident.tags.map((tag,index)=>{
-                        return  <span key={index}>{tag.name}</span>
-                    })}  </StyledBox>} 
+
+                    <StyledBox>Affected services:</StyledBox>{summaryIncident.tags.length!=0 &&<> <Box style={{borderRadius: '10px' ,border: '1px solid'+ theme.palette.info.main,width:"100%",height:"50px", padding:"1.5% "}} > 
+                    {summaryIncident.tags.map((tag, index) => {
+                        return   <Chip sx={{ border: '1px solid' + theme.palette.secondary.main, color: theme.palette.secondary.main, backgroundColor: theme.palette.secondary.light,margin:"0.5%"  }}  key={index} label={tag.name}/> 
+                    })} </Box> </> }
                 </Grid>
             </Grid>
         </StyledPaper>
     );
-};
+}; 
 
 export default DisplaySummary;
--- ims-server/src/routes/awsRouter.ts
+++ ims-server/src/routes/awsRouter.ts
@@ -6,7 +6,7 @@ const upload = multer({ dest: 'uploads/' });
 const awsRouter = express.Router()
 const awsController = new AwsController()
 
-awsRouter.get('/', awsController.getAllAttachmentByTimeline)
+// awsRouter.get('/', awsController.getAllAttachmentByTimeline)
 awsRouter.post('/', upload.array('files'), awsController.uploadAttachment)
 awsRouter.delete('/:key', awsController.deleteAttachmentById)
 
