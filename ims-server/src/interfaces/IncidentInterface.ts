b5be9d2dc01ba4d23401e13601d124457b867a89
tamarmiller1 <tamarm@darkeychana.com>
2023-07-25 17:52:10 +0300

    [IMS-178-B] -  create channel

--- ims-server/src/interfaces/IncidentInterface.ts
+++ ims-server/src/interfaces/IncidentInterface.ts
@@ -10,7 +10,7 @@ export interface IIncident {
   type: string;
   durationHours: number;
   channelId?:string;
-  slackLink: "string";
+  slackLink: string;
   channelName?: string;
   currentTags: ITag[];
   date: string;
