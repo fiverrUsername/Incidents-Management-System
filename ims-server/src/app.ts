09de0fca6ecdaf28d65d95b822b747ce8c3a322f
Beily123 <bk0527680141@gmail.com>
2023-07-25 13:07:01 +0300

    addCorsMiddleWare

--- ims-server/src/app.ts
+++ ims-server/src/app.ts
@@ -3,7 +3,6 @@ import cors from 'cors';
 import express, { Request, Response } from 'express';
 import fs from 'fs';
 import swaggerUI from 'swagger-ui-express';
-
 import config from './config/config';
 import logger from './loggers/log';
 import { connect } from './models/db';
@@ -12,22 +11,42 @@ import aggregationRouter from './routes/aggrigationRouter';
 import tagRouter from './routes/tagRouter';
 import timelineEventRouter from './routes/timelineEventRouter';
 import awsRouter from './routes/awsRouter';
+import { authenticateToken } from './middle_wares/authentication';
 const port = config.server.port
 
+const app = express()
 
 const swaggerFile: any = (process.cwd() + "/src/Swagger.json");
 const swaggerData: any = fs.readFileSync(swaggerFile, 'utf8');
 const swaggerDocument = JSON.parse(swaggerData);
 swaggerDocument.servers[0].url = `http://localhost:${process.env.SERVER_PORT}`
-const app = express()
+
+const whitelist = ['http://localhost:3000'];
+
+const corsOptions: cors.CorsOptions = {
+  origin: (origin, callback) => {
+    if (origin === undefined || whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
+
+
 connect()
+app.use(cors(corsOptions));
+app.use(authenticateToken);
+
 app.use('/swagger', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
-app.use(cors())
 app.use(bodyParser.json())
 app.use('/incident', incidentRoute)
 app.use('/aggregation', aggregationRouter)
 app.use('/tag', tagRouter)
-app.use('/timelineEvent',timelineEventRouter)
+app.use('/timelineEvent', timelineEventRouter)
 app.use('/aws', awsRouter)
 app.get('/', (req: Request, res: Response): void => {
   res.redirect('/swagger')
