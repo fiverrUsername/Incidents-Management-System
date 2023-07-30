e450b7df261ddc367dabd6f0c50218b440715833
ruth <ruthb9488@gmail.com>
2023-07-30 18:55:06 +0300

    [IMS-240] - listen to slack events

--- slack-server/src/app.ts
+++ slack-server/src/app.ts
@@ -3,19 +3,39 @@ import { addTimeLineEvent, ITimelineEvent } from "./services/slack-api/wrap/send
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import { sendMassageToSlack } from "./services/slack-api/actions/postMessage";
+import { createEventAdapter } from '@slack/events-api';
+import { WebClient } from '@slack/web-api';
 
 const app = express();
 const port = 4700;
 
+const slackEvents = createEventAdapter('sxzE9hZ9agbQmCgkADz8bJ9J');
+
 app.use(bodyParser.json());
-app.get('/',()=>console.log("ssts"))
+app.post('/webhook', (req, res) => {
+  const data = req.body;
+  console.log("data",data)
+ if (data.challenge) {
+   
+    const challenge = data.challenge;
+    res.json({ challenge });
+  } else {
+   
+   res.json({ message: 'Webhook event received successfully.' });
+  }
+});
+
 app.post('/', (req:Request, res:Response) => {
     console.log(req.body)
     addTimeLineEvent(req.body);
     res.json({ message: 'Timeline event added successfully'});
   });
 
-sendMassageToSlack({channelId:"C05JBDPBQ1H",userName:"Avigail",filesUrl:['C:/Users/user1/Documents/4e42471b0fe408436af17b42a5a122d5.jpg'],text:"exlain my incidents😂"});
   app.listen(port, () => {
-    console.log(`Server A listening on port ${port}`);
+    console.log(`Server is on http://localhost:${port}`)
   });
+const port1 = 4701; 
+slackEvents.start(port1).then(() => {
+  console.log('Server started!');
+});
+
