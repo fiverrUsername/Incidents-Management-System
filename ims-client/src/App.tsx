25930bf4c9af6bbbc86550b26784e9522fc91d67
אביטל פינץ <avital058pz@gmail.com>
2023-08-14 01:45:05 +0300

    [IMS-263 GRAF]-first commit

--- ims-client/src/App.tsx
+++ ims-client/src/App.tsx
@@ -8,6 +8,7 @@ import LeftDrawer, { IIcon } from "./components/drawer/Drawer";
 import theme from "./theme";
 import Router from "./routes";
 import Table from "./components/table/table";
+import HeatmapChar from "./components/heatmapChar/heatmapChar";
 function App() {
   const drawerIcons: IIcon[] = [
     { icon: BiHomeHeart, text: "home", navigation: "./" },
@@ -22,7 +23,8 @@ function App() {
           <LeftDrawer icons={drawerIcons} />
           <Box  component="main" sx={{ flexGrow: 1, p: 3, height: 'calc(100vh)', overflow: 'auto'  }}>
             {/* <Here put all the components /> */}
-            <Router/>
+            <HeatmapChar></HeatmapChar>
+            {/* <Router/> */}
             {/* <Attachmentlist _id={"3177dcba-2882-43f4-b809-897fcd162dc1"} />; */}
           </Box>
         </Box>
---
+++ ims-client/src/components/heatmapChar/heatmapChar.tsx
@@ -0,0 +1,131 @@
+import { ApexOptions } from 'apexcharts';
+import React from 'react';
+import ReactApexChart from 'react-apexcharts';
+
+const HeatmapChar = () => {
+
+  const options:ApexOptions= {
+    chart: {
+      width:600,
+      height: 300,
+      type: 'heatmap',
+    },
+    stroke:{
+
+    },
+    grid:{
+      xaxis: {
+        lines: {
+          offsetX: 12,
+          offsetY: 12
+        }
+      },
+      yaxis: {
+        lines: {
+          offsetX: 12,
+          offsetY: 12
+        }
+      },
+      row: {
+        opacity: 12
+      },
+      column: {
+        opacity: 12
+      },
+    
+    },
+    plotOptions: {
+      heatmap: {
+        shadeIntensity: 0.5,
+        colorScale: {
+          ranges: [
+            {
+              from: 0,
+              to: 20,
+              name: 'p3',
+              color: '#7fff00',
+            },
+            {
+              from: 21,
+              to: 45,
+              name: 'p2',
+              color: '#ffc000',
+            },
+            {
+              from: 46,
+              to: 55,
+              name: 'p1',
+              color: '#ff8000',
+            },
+            {
+              from: 56,
+              to: 100,
+              name: 'p0',
+              color: '#FF0000',
+            },
+          ],
+        },
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    xaxis: {
+      categories: ['03/08/2023', '04/08/2023', '05/08/2023', '06/08/2023'],
+    },
+    yaxis: {
+      labels: {
+        show: true,
+      },
+    },
+    tooltip:{
+      x: {
+        show: true,
+        format: 'dd MMM',
+        formatter: undefined,
+    },
+    y: {
+      formatter: undefined,
+      title: {
+          formatter: (seriesName) => seriesName,
+      },
+  },
+  z: {
+      formatter: undefined,
+      title: 'Size: '
+  },
+    }
+  }
+  
+
+ const series:ApexAxisChartSeries= [
+      {
+        name: 'Inbox',
+        data: [
+          { x: 'W1', y: 17 },
+          { x: 'W2', y: 100 },
+          { x: 'W3', y: 48 },
+          { x: 'W4', y: 32 },
+        ],
+      },
+      {
+        name: 'checkout',
+        data: [
+          { x: 'W1', y: 47 },
+          { x: 'W2', y: 29 },
+          { x: 'W3', y: 13 },
+          { x: 'W4', y: 100 },
+        ],
+      },
+    ]
+ 
+
+  return (
+    <div id="chart">
+      <ReactApexChart options={options} series={series} type={options.chart?.type} height={options.chart?.height} 
+      width={options.chart?.width} />
+    </div>
+  );
+};
+
+export default HeatmapChar;
