92f6f681c2844c9f2a49460effb2c78fd99e3b0a
אביטל פינץ <avital058pz@gmail.com>
2023-08-14 11:28:14 +0300

    change

--- ims-client/src/App.tsx
+++ ims-client/src/App.tsx
@@ -23,8 +23,7 @@ function App() {
           <LeftDrawer icons={drawerIcons} />
           <Box  component="main" sx={{ flexGrow: 1, p: 3, height: 'calc(100vh)', overflow: 'auto'  }}>
             {/* <Here put all the components /> */}
-            <HeatmapChar></HeatmapChar>
-            {/* <Router/> */}
+            <Router/>
             {/* <Attachmentlist _id={"3177dcba-2882-43f4-b809-897fcd162dc1"} />; */}
           </Box>
         </Box>
