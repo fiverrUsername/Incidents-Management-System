dfad0c8a3d67c07816eb263aff543e6574a7aa16
Ahuvi112 <ahuvigit@gmail.com>
2023-08-15 20:35:41 +0300

    [IMS-309] fix bug

--- ims-client/src/components/AddIncident/addIncidentComp.tsx
+++ ims-client/src/components/AddIncident/addIncidentComp.tsx
@@ -1,8 +1,12 @@
 import { Box, Button } from "@mui/material";
-import React,{ useState } from "react";
+import React, { useState } from "react";
 import AddIncident from "./addIncident";
 
-export default function AddIncidentComp() {
+export interface Props {
+    handleRefresh: () => void;
+  }
+
+export default function AddIncidentComp({handleRefresh}: Props) {
     const [open, setOpen] = useState(false);
     const handleClick = () => {
         setOpen(!open);
@@ -10,11 +14,13 @@ export default function AddIncidentComp() {
 
     const handleClose = () => {
         setOpen(false);
+        handleRefresh(); // כאשר הוא נסגר, נפעיל את הרענון בעמוד הראשי
     };
+    
     return (
         <div>
             <Button onClick={handleClick} variant='outlined'>+ Add New</Button>
-            {open && <AddIncident open={open} onClose={handleClose} />}   
+            {open && <AddIncident open={open} onClose={handleClose}/>}
         </div>
     )
 
