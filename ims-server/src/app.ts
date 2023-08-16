081d871c251ff61cc0d07de2579ec78036195424
NechamiSchenberg <ns0583229284+0@gmail.com>
2023-08-16 11:14:08 +0300

    ..

--- ims-server/src/app.ts
+++ ims-server/src/app.ts
@@ -37,18 +37,6 @@ const corsOptions: cors.CorsOptions = {
 
 connect();
 app.use(cors(corsOptions));
-// app.use(cors({
-//   origin: true, // "true" will copy the domain of the request back
-//   // to the reply. If you need more control than this
-//   // use a function.
-//   credentials: true, // This MUST be "true" if your endpoint is
-//   // authenticated via either a session cookie
-//   // or Authorization header. Otherwise the
-//   // browser will block the response.
-//   methods: 'POST,GET,PUT,OPTIONS,DELETE' // Make sure you're not blocking
-//   // pre-flight OPTIONS requests
-// }));
-// app.use(authenticateToken);
 
 app.use('/swagger', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use(bodyParser.json())
