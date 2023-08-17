7b201e549b9af7d6b976dfa15efa032eea87f1d3
tzivia-rot <tz3183761@gmail.com>
2023-08-17 17:51:14 +0300

    add icon

--- ims-client/src/components/attachment/attachment.tsx
+++ ims-client/src/components/attachment/attachment.tsx
@@ -14,6 +14,8 @@ import word from '../../images/word.jpg';
 import { IAttachmentData } from '../../interface/timeLineInterface';
 import attachmentService from '../../service/attachmentService';
 import { SingleAttachment, StyledImage } from './attachment.style';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFileWord } from '@fortawesome/free-solid-svg-icons';
 
 const getFileName = (fileName: string) => {
   const parts = fileName.split('_');
@@ -49,6 +51,10 @@ export default function Attachment({
       }
     };
 
+    useEffect(() => {
+      fetchDownloadUrl()
+     }, []);
+
   useEffect(() => {
    if(downloadUrl!=null)
       renderFileContent()
@@ -77,17 +83,19 @@ export default function Attachment({
     // const fileURL = URL.createObjectURL(fileBlob);
     // // Create a download link
     // console.log(fileURL)
+    let url:string;
     try {
-      const response = await attachmentService.getUrl(file);
-
-      setDownloadUrl(response);
+    const response = await attachmentService.getUrl(file);
+    setDownloadUrl(response);
+    url=response;
+    const downloadLink = document.createElement("a");
+    downloadLink.href = url;
+    downloadLink.download = file;
+    downloadLink.click();
     } catch (error) {
       console.log(error);
     }
-    const downloadLink = document.createElement("a");
-    downloadLink.href = downloadUrl;
-    downloadLink.download = file;
-    downloadLink.click();
+    
     // console.log(file.data)
     // download(file.data, getFileName(file.key))
   };
@@ -110,22 +118,20 @@ export default function Attachment({
 
 
   const renderFileContent = () => {
-    
-    if(!downloadUrl)
+    if(fileType=='image'&&!downloadUrl)
       return null;
     if (!file) {
       return null;
     }
-    console.log("fileType",fileType);
     switch (fileType) {
       case 'image':
         // return renderImageContent();
       // return  <a href={downloadUrl} download>
       //   Download File
       // </a>;
-      return <iframe src={downloadUrl} width="100%" height="500px" title="File Viewer" />
+      return <StyledImage src={downloadUrl} width={100} title="File Viewer" />
       case 'pdf':
-        return <img src={pdf} alt="pdf" title={getFileName(file)} />;
+        return <StyledImage src={pdf} alt="audio" title={getFileName(file)} />;
       case 'txt':
         return <StyledImage src={txt} alt="audio" title={getFileName(file)} />;
       case 'audio':
@@ -133,7 +139,7 @@ export default function Attachment({
       case 'video':
         return <StyledImage src={video} alt="video" title={getFileName(file)} />;
       case 'word':
-        return <StyledImage src={word} alt="word" title={getFileName(file)} />;
+        return <div><FontAwesomeIcon icon={faFileWord}/></div>
       case 'powerpoint':
         return <StyledImage src={PowerPoint} alt="powerPoint" title={getFileName(file)} />;
       case 'excel':
