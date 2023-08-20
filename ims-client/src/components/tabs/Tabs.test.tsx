bf620f4d75e92bbb293600766c1bc465da482d74
Ahuvi112 <ahuvigit@gmail.com>
2023-08-20 09:29:58 +0300

    [IMS-323] Refctor -Client - table

--- ims-client/src/components/tabs/Tabs.test.tsx
+++ ims-client/src/components/tabs/Tabs.test.tsx
@@ -4,35 +4,35 @@ import Tabs from './Tabs';
 import theme from '../../theme';
 
 describe('Tabs', () => {
-  test('renders the component', () => {
-    render(<Tabs onEvent={jest.fn()} setValue={jest.fn()} />);
-  });
+    test('renders the component', () => {
+        render(<Tabs setValue={jest.fn()} />);
+    });
 
-  test('changes the active tab on click', () => {
-    const { getByText } = render(<Tabs onEvent={jest.fn()} setValue={jest.fn()} />);
-    const activeTab = getByText('Active Incidents');
-    const solvedTab = getByText('Solved Incidents');
+    test('changes the active tab on click', () => {
+        const { getByText } = render(<Tabs setValue={jest.fn()} />);
+        const activeTab = getByText('Active Incidents');
+        const solvedTab = getByText('Solved Incidents');
 
-    fireEvent.click(solvedTab);
+        fireEvent.click(solvedTab);
 
-    expect(solvedTab).toHaveStyle(`backgroundColor: ${theme.palette.secondary.main}`);
-    expect(activeTab).not.toHaveStyle(`backgroundColor: ${theme.palette.secondary.main}`);
-  });
+        expect(solvedTab).toHaveStyle(`backgroundColor: ${theme.palette.secondary.main}`);
+        expect(activeTab).not.toHaveStyle(`backgroundColor: ${theme.palette.secondary.main}`);
+    });
 
-  test('calls the onEvent function with the correct value when the tab changes', () => {
-    const mockOnEvent = jest.fn();
-    const { getByText } = render(<Tabs onEvent={mockOnEvent} setValue={jest.fn()} />);
-    const activeTab = getByText('Active Incidents');
-    const solvedTab = getByText('Solved Incidents');
+    test('calls the onEvent function with the correct value when the tab changes', () => {
+        const mockOnEvent = jest.fn();
+        const { getByText } = render(<Tabs setValue={jest.fn()} />);
+        const activeTab = getByText('Active Incidents');
+        const solvedTab = getByText('Solved Incidents');
 
-    fireEvent.click(solvedTab);
+        fireEvent.click(solvedTab);
 
-    expect(mockOnEvent).toHaveBeenCalledTimes(1);
-    expect(mockOnEvent).toHaveBeenCalledWith('someFunction');
+        expect(mockOnEvent).toHaveBeenCalledTimes(1);
+        expect(mockOnEvent).toHaveBeenCalledWith('someFunction');
 
-    fireEvent.click(activeTab);
+        fireEvent.click(activeTab);
 
-    expect(mockOnEvent).toHaveBeenCalledTimes(2);
-    expect(mockOnEvent).toHaveBeenCalledWith('someFunction');
-  });
+        expect(mockOnEvent).toHaveBeenCalledTimes(2);
+        expect(mockOnEvent).toHaveBeenCalledWith('someFunction');
+    });
 });
