fd99964079f34298f220dce485ee8c34b6df8dbf
tzivia-rot <tz3183761@gmail.com>
2023-08-21 15:42:24 +0300

    delete function

--- ims-client/src/service/attachmentService.ts
+++ ims-client/src/service/attachmentService.ts
@@ -7,7 +7,7 @@ const attachmentService = {
     headers: {
       'Content-Type': 'multipart/form-data',
     },
-  }).then((response) => response.data()).then(),
+  }).then((response) => response.data()),
 
   showAttachment: (files: string[]) => axios.post(`${baseUrl}/attachment/allAttachments`, files, {
     // responseType:'blob'
