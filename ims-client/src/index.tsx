ce37ea38c9d9600755f0117d9ae4d900c01b8b3b
ChanaLipsky <chanalip7@gmail.com>
2023-08-27 14:57:17 +0300

    update index.tsx

--- ims-client/src/index.tsx
+++ ims-client/src/index.tsx
@@ -1,19 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import './index.css'
+// import App from './App'
+// import reportWebVitals from './reportWebVitals'
+// import { BrowserRouter, HashRouter } from 'react-router-dom'
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// )
+// root.render(
+//   //  <React.StrictMode>
+//     <HashRouter>
+//       <App />
+//     </HashRouter>
+//   // </React.StrictMode> 
+// )
+
+// reportWebVitals()
+
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { HashRouter } from 'react-router-dom';
+import { RouterProvider, createHashRouter } from 'react-router-dom'; // Import necessary components
+import { createRoot } from 'react-dom/client';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 )
-root.render(
-  //  <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  // </React.StrictMode> 
-)
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
 
-reportWebVitals()
+root.render(
+  <React.StrictMode>
+    <HashRouter>
+      <RouterProvider router={router}/>
+    </HashRouter>
+  </React.StrictMode>
+);
+
+export { router, root };
