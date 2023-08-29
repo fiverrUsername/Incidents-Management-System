f19125a54987203df0045cc385b64a87155b8f88
Rivky Fisher <hand1b20@mbjcomp.org.il>
2023-08-29 13:45:56 +0300

    HashRouter

--- ims-client/src/routes.tsx
+++ ims-client/src/routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes,HashRouter } from 'react-router-dom';
 
 import { withId } from './HOC';
 import IncidentsPage from './pages/incidents/incidentsPage';
@@ -9,13 +9,13 @@ import TimeLinePage from './pages/timeLine/timeLinePage';
 
 export default function Router() {
   return (
-    <Routes>
+    <HashRouter>
       <Route path="/incident" element={<IncidentsPage />} />
       <Route path="/timeline/:id" Component={withId(TimeLinePage)} />
       <Route path="/" element={<Navigate to="/incident" />} />
       <Route path="*" element={<h1>404 Page not found 🤔</h1>} />
       <Route path="/liveStatus" element={<LiveStatus />} />
-    </Routes>
+    </HashRouter>
   );
 }
 
