cceadd8e7ca9cfc1a55363ca0708cb9588092f8e
tzivia-rot <tz3183761@gmail.com>
2023-08-31 13:11:44 +0300

    add config to s3

--- ims-server/src/repositories/attachmentRepository.ts
+++ ims-server/src/repositories/attachmentRepository.ts
@@ -9,6 +9,13 @@ const s3 = new AWS.S3();
 const expiration = 3600;
 
 
+AWS.config.getCredentials(function(err) {
+  if (err) console.log(err.stack);
+  // credentials not loaded
+  else {
+    console.log("Access key:", AWS.config.credentials?.accessKeyId);
+  }
+});
 class AttachmentsRepository {
 
   async uploadAttachment(files: Express.Multer.File[]): Promise<AWS.S3.ManagedUpload.SendData | any> {
