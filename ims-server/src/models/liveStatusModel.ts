ac1a43366aa4a944114e32358fe47b5537e2c402
margalithager <margalit.0567@gmail.com>
2023-08-31 17:29:16 +0300

    add the index to live status

--- ims-server/src/models/liveStatusModel.ts
+++ ims-server/src/models/liveStatusModel.ts
@@ -1,9 +1,48 @@
+// import mongoose, { Schema } from "mongoose";
+// import { v4 as uuidv4 } from "uuid";
+// import { IliveStatus } from "../interfaces/liveStatusInterface";
+// import { Priority } from "../enums/enum";
+
+// export const liveStatusSchema = new Schema<IliveStatus>({
+//     id: {
+//         type: String,
+//         default: uuidv4,
+//     },
+//     systemName: {
+//         type: String,
+//         required: true
+//     },
+//     incidents: {
+//         type: [[String]],
+//         required: true
+//     },
+//     date: {
+//         type: Date,
+//         required: true
+//     },
+//     maxPriority: {
+//         type: String,
+//         required: true,
+//         enum: Object.values(Priority),
+//     },
+//     incidentCounter: {
+//         type: Number,
+//         required: true,
+//     },
+//     resolvedIncidents: {
+//         type: Number,
+//         required: true,
+//     }
+// });
+// export default mongoose.model<IliveStatus>("liveStatus", liveStatusSchema);
+
+
 import mongoose, { Schema } from "mongoose";
 import { v4 as uuidv4 } from "uuid";
 import { IliveStatus } from "../interfaces/liveStatusInterface";
 import { Priority } from "../enums/enum";
 
-export const liveStatusSchema = new Schema<IliveStatus>({
+const liveStatusSchema = new Schema<IliveStatus>({
     id: {
         type: String,
         default: uuidv4,
@@ -34,4 +73,9 @@ export const liveStatusSchema = new Schema<IliveStatus>({
         required: true,
     }
 });
-export default mongoose.model<IliveStatus>("liveStatus", liveStatusSchema);
+
+liveStatusSchema.index({ date: 1 }); // הוספת האינדקס על שדה ה-"date"
+
+const LiveStatus = mongoose.model<IliveStatus>("liveStatus", liveStatusSchema);
+
+export default LiveStatus;
