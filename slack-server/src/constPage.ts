957bf61cc70bdef4b699e6c3b89f987866f40c4e
tamarmiller1 <tamarm@darkeychana.com>
2023-08-31 11:42:24 +0300

    chage to take from env

--- slack-server/src/constPage.ts
+++ slack-server/src/constPage.ts
@@ -10,9 +10,6 @@ export const wsPort = 'wss://ims-socket.onrender.com';
 
 export const WEBHOOK_EVENT_RECEIVED_SUCCESSFULLY = 'Webhook event received successfully.'
 export const TIMELINE_EVENT_ADDED_SUCCESSFULLY = 'Timeline event added successfully';
-
-
-
 export const SLACK_UPLOAD_FILES = 'https://slack.com/api/files.upload';
 export const NO_CHANNEL_NAME = 'NO CHANNEL NAME';
 export const NO_INCIDENT_NAME = 'NO INCIDENT NAME';
@@ -21,10 +18,10 @@ export const CHANNEL_REDIRECT = "https://slack.com/app_redirect?channel="
 
 
 
-export const SLACK_API_TOKEN ="xoxb-5609511342163-5604717800598-YFR1sA1lGrT6jFqw1kdlz7tN"
-export const SLACK_TOKEN= "xoxe.xoxp-1-Mi0yLTU2MDk1MTEzNDIxNjMtNTYwOTY2OTQ0NDE3OS01NTk1NjA3NDgyMDA3LTU4MjY2MDgwODAzMjUtMzNhNGUzYjRhY2JmYmIwZWNjZTdlMDViOGExYzA3ZjZiMTEzNWQ1ZGNiOWU1ODgxMjU5Mjk5ZDExMGJjNGU2ZA"
+// export const SLACK_API_TOKEN ="xoxb-5609511342163-5604717800598-YFR1sA1lGrT6jFqw1kdlz7tN"
+// export const SLACK_TOKEN= "xoxe.xoxp-1-Mi0yLTU2MDk1MTEzNDIxNjMtNTYwOTY2OTQ0NDE3OS01NTk1NjA3NDgyMDA3LTU4MjY2MDgwODAzMjUtMzNhNGUzYjRhY2JmYmIwZWNjZTdlMDViOGExYzA3ZjZiMTEzNWQ1ZGNiOWU1ODgxMjU5Mjk5ZDExMGJjNGU2ZA"
 
 
-export const client = new WebClient(SLACK_API_TOKEN, {
+export const client = new WebClient(process.env.SLACK_API_TOKEN, {
   logLevel: LogLevel.DEBUG,
 });
