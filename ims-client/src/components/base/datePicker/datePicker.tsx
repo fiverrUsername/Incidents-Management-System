d5d1688f48f5f70391ce89e05daa39ed76a843ae
dvora-ba <dvora0742@gmail.com>
2023-08-31 14:03:04 +0300

    [ims-380]

--- ims-client/src/components/base/datePicker/datePicker.tsx
+++ ims-client/src/components/base/datePicker/datePicker.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import dayjs, { Dayjs } from 'dayjs';
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
--- ims-client/src/components/base/dropDown/DropDown.tsx
+++ ims-client/src/components/base/dropDown/DropDown.tsx
@@ -1,4 +1,5 @@
-import FormControl from '@mui/material/FormControl';
+import * as React from 'react';
+import MenuItem from '@mui/material/MenuItem';
 import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
@@ -39,18 +40,3 @@ export default function DropDown(props:DropDownProps) {
         </FormControl>
     );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
