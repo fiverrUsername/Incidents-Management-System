c8a92092436e6daf49806cc9ffd6ba97d66c7b75
ShiraVaknin <shvaknin0634@gmail.com>
2023-08-31 18:07:40 +0300

    fix constants page

--- slack-server/src/actions/via-slack/fileResponse.ts
+++ slack-server/src/actions/via-slack/fileResponse.ts
@@ -1,11 +1,22 @@
 import axios from "axios";
 import AWS from 'aws-sdk';
 import FormData from 'form-data';
-import fs from 'fs'
-const s3 = new AWS.S3();
+import logger from "../../loggers/log";
+import { constants } from "../../loggers/constants";
+
+const s3 = new AWS.S3({
+  region: process.env.AWS_REGION,
+});
+AWS.config.getCredentials(function(err) {
+  if (err) console.log(err.stack);
+  // credentials not loaded
+  else {
+    logger.info({ source: constants.ACCESS_KEY, msg: AWS.config.credentials?.accessKeyId, success: true });
+    console.log("Access key:", AWS.config.credentials?.accessKeyId);
+  }
+});
 export async function fileResponse(files: any[], incidentId: string): Promise<string[]> {
   const filesKeys: string[] = [];
-  const formData = new FormData();
   try {
     await Promise.all(files.map(async (file) => {
       try {
@@ -28,13 +39,16 @@ export async function fileResponse(files: any[], incidentId: string): Promise<st
         };
         console.log("##########################params.body", params.Body);
         await s3.upload(params).promise();
+        console.log("UPLOAD_SUCCESS ")
+        logger.info({ source: constants.UPLOAD_SUCCESS, success: true });
       } catch (error) {
+        logger.error({ source: constants.UPLOAD_FAILED, msg: error});
         console.error("Error processing file:", error);
       }
     }));
     return filesKeys;
   } catch (error: any) {
-    console.error("ERROR_EXTRACTING_FILES", error.message);
+    logger.error({ source: constants.ERROR_EXTRACTING_FILES, msg: error });
     return [];
   }
 }
