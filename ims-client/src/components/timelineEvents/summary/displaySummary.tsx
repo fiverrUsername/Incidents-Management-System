d6587c98737965783557de2d9b503d8a62295aa0
malkyfried <hand1b22@bjcomp.org>
2023-09-03 02:08:19 +0300

    Fix render in summary,updating every minute

--- ims-client/src/components/timelineEvents/summary/displaySummary.tsx
+++ ims-client/src/components/timelineEvents/summary/displaySummary.tsx
@@ -1,23 +1,34 @@
-import { Box, Chip, Grid, } from '@mui/material';
+import { Box, Chip, Grid } from '@mui/material';
 import dayjs from 'dayjs';
 import React, { useEffect, useState } from 'react';
 import { ISummary } from '../../../interfaces/ISummary';
 import { StyledBox, StyledPaper } from '../../../pages/timeLine/timeLinePage.style';
 import theme from '../../../theme';
 import backendServices from '../../../services/backendServices/backendServices';
+import log from '../../../loggers/logger'
 
 interface propsDisplaySummary {
     id: string
 }
+
 const DisplaySummary = ({ id }: propsDisplaySummary) => {
-    const [summaryIncident, setSummaryIncident] = useState<ISummary>();
-    useEffect(() => {
-        const fetchSummaryIncident = async () => {
+    const [summaryIncident, setSummaryIncident] = useState<ISummary | null>(null);
+    
+    const fetchData = async () => {
+        try {
             const summary = await backendServices.getSummaryIncident(id);
-            setSummaryIncident(summary)
+            setSummaryIncident(summary);
+        } catch (error) {
+            log.error({message:"failed to fetch summary",source:"displaySummary"});
         }
-        fetchSummaryIncident();
-    }, [id, summaryIncident]);
+    };
+
+    useEffect(() => {
+        fetchData();
+        const intervalId = setInterval(fetchData, 60000); // 60000 milliseconds = 1 minute
+        return () => clearInterval(intervalId);
+    }, [id]);
+
     const date = summaryIncident?.createdAt
         ? dayjs(summaryIncident.createdAt).format("DD/MM/YYYY")
         : "";
@@ -35,17 +46,19 @@ const DisplaySummary = ({ id }: propsDisplaySummary) => {
                         <StyledBox>Current priority: <label style={{ color: theme.palette.primary.dark, fontSize: theme.typography.fontSize }}>{summaryIncident.currentPriority}</label></StyledBox>
                     </Grid>
                     <Grid container direction="column" justifyContent="flex-start" alignItems="flex-start">
-
                         <StyledBox>Affected services:</StyledBox>
-                        {summaryIncident.tags.length != 0 && <>
-                            <Box style={{ borderRadius: '10px', border: '1px solid' + theme.palette.info.main, width: "100%", height: "auto", padding: "1.5% " }} >
-                                {summaryIncident.tags.map((tag, index) => {
-                                    return <Chip sx={{ border: '1px solid' + theme.palette.secondary.main, color: theme.palette.secondary.main, backgroundColor: theme.palette.secondary.light, margin: "0.5%" }} key={index} label={tag.name} />
-                                })} </Box> </>}
+                        {summaryIncident.tags.length !== 0 && (
+                            <Box style={{ borderRadius: '10px', border: '1px solid' + theme.palette.info.main, width: "100%", height: "auto", padding: "1.5% " }}>
+                                {summaryIncident.tags.map((tag, index) => (
+                                    <Chip sx={{ border: '1px solid' + theme.palette.secondary.main, color: theme.palette.secondary.main, backgroundColor: theme.palette.secondary.light, margin: "0.5%" }} key={index} label={tag.name} />
+                                ))}
+                            </Box>
+                        )}
                     </Grid>
                 </Grid>
             )}
         </StyledPaper>
     );
 };
+
 export default DisplaySummary;
